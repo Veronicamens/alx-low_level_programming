ccd5063e722c78ab0ad04552d40aadcb22c0c0be
Veronicamens <odamttenveronica@gmail.com>
2023-07-06 10:17:02 -0700

    Writing a function that checks the endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,16 +2,15 @@
 
 /**
 * get_endianness - check endianness
-* Return: 0 if little endian, or 1 if big endian
+* Return: 0 if big endian, or 1 if little endian
 */
 int get_endianness(void)
 {
-
 int num;
-num = 256;
+
+num = 1;
 if (*(char *)&num == 1)
 return (1);
 else
 return (0);
-
 }
